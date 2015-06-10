c9b55365a1203e0e8053f53306954d824c73ced2
M-Tajima <fj0012ir@aa.jp.fujitsu.com>
2015-06-10 12:19:41 +0900

    テストで出力するログの出力先を変更

--- circle.sh
+++ circle.sh
@@ -3,20 +3,21 @@ case "$1" in
     case $CIRCLE_NODE_INDEX in
       0)
         # run common tests
-        mvn site -B -pl common -Ddependency.locations.enabled=false > artifacts/log/common-site.log
+        mvn site -B -pl common -Ddependency.locations.enabled=false > $CIRCLE_ARTIFACTS/common-site.log
 
         ;;
 
       1)
         # run core
         # regist data for test
-        mvn test -B -pl core -Dtest=com.fujitsu.dc.test.setup.Setup#reset > artifacts/log/core-reset.log
-        mvn test -B -pl core -Dtest=com.fujitsu.dc.test.setup.Setup#resetEventLog > artifacts/log/core-resetEventLog.log
-        mvn site -B -pl core -Ddependency.locations.enabled=false -Dtest=com.fujitsu.dc.test.jersey.box.odatacol > artifacts/log/core-site.log
+        mvn test -B -pl core -Dtest=com.fujitsu.dc.test.setup.Setup#reset > $CIRCLE_ARTIFACTS/core-reset.log
+        mvn test -B -pl core -Dtest=com.fujitsu.dc.test.setup.Setup#resetEventLog > $CIRCLE_ARTIFACTS/core-resetEventLog.log
+        mvn site -B -pl core -Ddependency.locations.enabled=false -Dtest=com.fujitsu.dc.test.jersey.box.odatacol > $CIRCLE_ARTIFACTS/core-site.log
 
         ;;
 
     esac
 
     ;;
+
 esac
