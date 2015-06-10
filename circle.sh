d7205fa7d411cb5f0e5f9df2f78033719e007e83
M-Tajima <fj0012ir@aa.jp.fujitsu.com>
2015-06-10 15:10:41 +0900

    coreのテスト用の前準備を関数化した

--- circle.sh
+++ circle.sh
@@ -1,3 +1,12 @@
+function setupForCoreTest() {
+  # copy configuration file for CI environment
+  cp -p core/src/test/resources/ci/dc-config.properties core/src/test/resources/dc-config.properties
+
+  # regist data for test
+  mvn test -B -pl core -Dtest=com.fujitsu.dc.test.setup.Setup#reset > $CIRCLE_ARTIFACTS/core-reset.log
+  mvn test -B -pl core -Dtest=com.fujitsu.dc.test.setup.Setup#resetEventLog > $CIRCLE_ARTIFACTS/core-resetEventLog.log
+}
+
 case "$1" in
   test)
     case $CIRCLE_NODE_INDEX in
@@ -9,13 +18,7 @@ case "$1" in
 
       1)
         # run core that included in com.fujitsu.dc.core except odatacol
-
-        # copy configuration file for CI environment
-        cp -p core/src/test/resources/ci/dc-config.properties core/src/test/resources/dc-config.properties
-
-        # regist data for test
-        mvn test -B -pl core -Dtest=com.fujitsu.dc.test.setup.Setup#reset > $CIRCLE_ARTIFACTS/core-reset.log
-        mvn test -B -pl core -Dtest=com.fujitsu.dc.test.setup.Setup#resetEventLog > $CIRCLE_ARTIFACTS/core-resetEventLog.log
+        setupForCoreTest
 
         # exclude test
         echo "com.fujitsu.dc.test.*" >> .test-excludes
@@ -27,13 +30,7 @@ case "$1" in
 
       2)
         # run core that included in com.fujitsu.dc.test except odatacol
-
-        # copy configuration file for CI environment
-        cp -p core/src/test/resources/ci/dc-config.properties core/src/test/resources/dc-config.properties
-
-        # regist data for test
-        mvn test -B -pl core -Dtest=com.fujitsu.dc.test.setup.Setup#reset > $CIRCLE_ARTIFACTS/core-reset.log
-        mvn test -B -pl core -Dtest=com.fujitsu.dc.test.setup.Setup#resetEventLog > $CIRCLE_ARTIFACTS/core-resetEventLog.log
+        setupForCoreTest
 
         # exclude test
         echo "com.fujitsu.dc.core.*" >> .test-excludes
@@ -45,13 +42,7 @@ case "$1" in
 
       3)
         # run core that included in com.fujitsu.dc.test.jersey.box.odatacol
-
-        # copy configuration file for CI environment
-        cp -p core/src/test/resources/ci/dc-config.properties core/src/test/resources/dc-config.properties
-
-        # regist data for test
-        mvn test -B -pl core -Dtest=com.fujitsu.dc.test.setup.Setup#reset > $CIRCLE_ARTIFACTS/core-reset.log
-        mvn test -B -pl core -Dtest=com.fujitsu.dc.test.setup.Setup#resetEventLog > $CIRCLE_ARTIFACTS/core-resetEventLog.log
+        setupForCoreTest
 
         # exclude test
         echo "com.fujitsu.dc.core.*" >> .test-excludes
