e3e293584ecbb06ea339e8080d89f0b59dcee2c8
M-Tajima <fj0012ir@aa.jp.fujitsu.com>
2015-06-10 11:34:32 +0900

    debug用のメッセージを追加

--- circle.sh
+++ circle.sh
@@ -2,12 +2,14 @@ case "$1" in
   test)
     case $CIRCLE_NODE_INDEX in
       0)
+        echo pwd: `pwd`
         # run common tests
         mvn site -B -pl common -Ddependency.locations.enabled=false > artifacts/log/common-site.log
 
         ;;
 
       1)
+        echo pwd: `pwd`
         # run core
         # regist data for test
         mvn test -B -pl core -Dtest=com.fujitsu.dc.test.setup.Setup#reset > artifacts/log/core-reset.log
