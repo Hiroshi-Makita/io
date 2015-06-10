3c9bf9942666b1653bb09578e7bc3aee4d3b348e
M-Tajima <fj0012ir@aa.jp.fujitsu.com>
2015-06-10 16:13:24 +0900

    シェルにCI設定を外出した

--- circle.sh
+++ circle.sh
@@ -8,6 +8,43 @@ function setupForCoreTest() {
 }
 
 case "$1" in
+  pre_machine)
+    # ensure correct level of parallelism
+    expected_nodes=4
+    if [ "$CIRCLE_NODE_TOTAL" -ne "$expected_nodes" ]
+    then
+        echo "Parallelism is set to ${CIRCLE_NODE_TOTAL}x, but we need ${expected_nodes}x."
+        exit 1
+    fi
+
+    ;;
+
+  dependencies)
+    mvn install -B -pl es-api,common,logback -Dmaven.javadoc.skip=true -Dmaven.test.skip=true
+
+    ;;
+
+  dependencies_post)
+    # elasticsearch
+    wget https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.3.4.tar.gz
+    tar -xvf elasticsearch-1.3.4.tar.gz
+    cp -p core/src/test/resources/ci/elasticsearch.yml elasticsearch-1.3.4/config/elasticsearch.yml
+    elasticsearch-1.3.4/bin/elasticsearch: {background: true}
+
+    # MySQL
+    sudo apt-add-repository -y 'deb http://ppa.launchpad.net/ondrej/mysql-experimental/ubuntu precise main'
+    sudo apt-get update; sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server-5.5
+    sudo cp -p core/src/test/resources/ci/my.cnf /etc/mysql/my.cnf
+    sudo service mysql restart
+
+    # create directory
+    sudo mkdir -p /fjnfs/dc-core/{dav,eventlog,barInstall}
+    sudo chown -R ubuntu.ubuntu /fjnfs
+    sudo mkdir -p /fj/logback/log/
+    sudo chown -R ubuntu.ubuntu /fj
+
+    ;;
+
   test)
     case $CIRCLE_NODE_INDEX in
       0)
@@ -54,7 +91,10 @@ case "$1" in
 
     esac
 
+    # save result of site
     find . -maxdepth 1 -type d ! -name "." -exec mv {}/target/site $CIRCLE_ARTIFACTS/{} \;
+    # save result of junit
+    # find . -type f -regex ".*/target/surefire-reports/.*xml" -exec cp {} $CIRCLE_TEST_REPORTS/junit/ \;
 
     ;;
 
